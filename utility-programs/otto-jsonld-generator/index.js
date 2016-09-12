1d235d383dc6d603989fe7d533ee14f9398a0eae
Meghna <ldeveloperl1985@gmail.com>
2016-09-12 20:43:55 +0530

    Adding web interface form to generate the template

--- utility-programs/otto-jsonld-generator/index.js
+++ utility-programs/otto-jsonld-generator/index.js
@@ -8,12 +8,12 @@ var jsonSignature = path.join(__dirname, './jsonSignature.js');
 var dataset = path.join(__dirname, './dataset.json');
 var settings = require('./config/settings');
 
-
 /****************Setting Middle ware to fetch the data ********************/
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
 
 function generateUUID() {
     function s4() {
@@ -36,21 +36,18 @@ app.post('/process_post', function(req, res) {
     var template = req.body.template;
     var counter = req.body.counter;
     var guidIdsrep = req.body.guidIdsrep;
-
     var guidIdsrepArr = guidIdsrep.split(",");
     var datasetArray = [];
 
     for (var i = 0; i < counter; i++) {
       var dataTemplate = template;
       // Replacing Guids
-
       for(var j=0;j<guidIdsrepArr.length;j++)
       {
           if(guidIdsrepArr[j] != null  && guidIdsrepArr[j].trim() != "" )
           {
             var regex = new RegExp(guidIdsrepArr[j], 'g');
             dataTemplate= dataTemplate.replace(regex,generateUUID);
-          
           }
       }
       datasetArray.push(JSON.parse(dataTemplate));
---
+++ utility-programs/otto-jsonld-generator/views/scripts/template.js
@@ -0,0 +1,100 @@
+var federationTemplate = {
+	"@id": "https://ra.org/federations/{fid}",
+	"@context": "https://ra.org/schema/otto/federation.jsonld",
+	"name": "OAuth 2 Federation",
+	"entities": [
+		{
+			"@context": "https://ra.org/schema/otto/entity/connect_rp.jsonld",
+			"@id": "https://ra.org/entity/{eid1}",
+			"name": "Gluu Server Ce-dev Client",
+			"id": "https://ce-dev.gluu.org/rp",
+			"organization": "https://gluu.org/otto/organization"
+		},
+		{
+			"@context": "https://ra.org/schema/otto/entity/connect_op.jsonld",
+			"@id": "https://ra.org/entity/{eid2}",
+			"name": "Gluu Server Ce-dev",
+			"id": "https://ce-dev.gluu.org",
+			"organization": "https://gluu.org/otto/organization"
+		},
+		{
+			"@context": "https://ra.org/schema/otto/entity/uma_rs.jsonld",
+			"@id": "https://ra.org/entity/{eid3}",
+			"name": "Gluu Resource Server",
+			"id": "https://ce-dev.gluu.org/rs",
+			"organization": "https://gluu.org/otto/organization"
+		}
+	],
+	"federations": [
+		"https://ra.org/federations/{fid}"
+	],
+	"organization": {
+		"@id": "https://ra.org/organization/{oid}",
+		"@context": "https://ra.org/schema/otto/organization.jsonld",
+		"name": "MyOrganization"
+	}
+};
+
+
+var organizationTemplate ={
+  "@id":"https://ra.org/organization/{oid}",
+  "@context": "https://schema.org",
+  "@type":"Organization",
+   "name":"MyOrganization",
+};
+
+var openIDConnectOPTemplate ={
+  "@id":"https://ra.org/federation_entity/{eid}",
+  "@context": "https://ra.org/schema/otto/entity/connect_op.jsonld",
+  "name": "Gluu Server Ce-dev",
+  "id":"https://ce-dev.gluu.org",
+  "organization":"https://gluu.org/otto/organization"
+};
+
+var openIDConnectRPTemplate ={
+  "@id":"https://ra.org/federation_entity/{eid}",
+  "@context": "https://ra.org/schema/otto/entity/connect_rp.jsonld",
+  "name": "Gluu Server Ce-dev Client",
+  "id":"https://ce-dev.gluu.org/rp",
+  "organization":"https://gluu.org/otto/organization"
+};
+
+var umaRSTemplate ={
+  "@id":"https://ra.org/federation_entity/{eid}",
+  "@context": "https://ra.org/schema/otto/entity/uma_rs.jsonld",
+  "name": "Gluu Resource Server",
+  "id":"https://ce-dev.gluu.org/rs",
+  "organization":"https://gluu.org/otto/organization"
+};
+
+var umaASTemplate={
+  "@id":"https://ra.org/federation_entity/{eid}",
+  "@context": "https://ra.org/schema/otto/entity/uma_as.jsonld",
+  "name": "Gluu AS",
+  "id":"https://ce-dev.gluu.org/as",
+  "organization":"https://gluu.org/otto/organization"
+};
+
+var umaROTemplate = {
+  "@id":"https://ra.org/federation_entity/{eid}",
+  "@context": "https://ra.org/schema/otto/entity/uma_ro.jsonld",
+  "name": "Gluu RO",
+  "id":"https://ce-dev.gluu.org/ro",
+  "organization":"https://gluu.org/otto/organization"
+};
+
+zvar oauth2OPTemplate ={
+  "@id":"https://ra.org/federation_entity/{eid}",
+  "@context": "https://ra.org/schema/otto/entity/oauth_op.jsonld",
+  "name": "Gluu oauth provider",
+  "id":"https://ce-dev.gluu.org/oauth_op",
+  "organization":"https://gluu.org/otto/organization"
+};
+
+var oauth2RPTemplate={
+  "@id":"https://ra.org/federation_entity/{eid}",
+  "@context": "https://ra.org/schema/otto/entity/oauth_rp.jsonld",
+  "name": "Gluu oauth rp",
+  "id":"https://ce-dev.gluu.org/oauth_rp",
+  "organization":"https://gluu.org/otto/organization"
+};
