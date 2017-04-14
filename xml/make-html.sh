630378a99a70d444dc324cb0ed339092b6b5d864
Mike Schwartz <mike@gluu.org>
2017-04-13 22:50:41 -0500

    OTTO Badge

--- xml/make-html.sh
+++ xml/make-html.sh
@@ -6,5 +6,5 @@ xsltproc rfc2629.xslt otto-api-01.xml > otto-api-1.0.html
 
 xsltproc rfc2629.xslt otto-openid-1.0.xml > otto-openid-1.0.html
 
-xsltproc rfc2629.xslt otto-badge-1.0.xml > otto-openid-1.0.html
+xsltproc rfc2629.xslt otto-badge-1.0.xml > otto-badge-1.0.html
 
