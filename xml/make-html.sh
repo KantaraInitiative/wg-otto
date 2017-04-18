e9d9c2a4eb6871414a615b80350a8db7399c541e
Mike Schwartz <mike@gluu.org>
2017-04-18 17:38:16 -0500

    Rendered html

--- xml/make-html.sh
+++ xml/make-html.sh
@@ -6,4 +6,3 @@ xsltproc rfc2629.xslt otto-api-01.xml > ../html/otto-api-1.0.html
 
 xsltproc rfc2629.xslt otto-openid-1.0.xml > ../html/otto-openid-1.0.html
 
-xsltproc rfc2629.xslt otto-badge-1.0.xml > ../html/otto-badge-1.0.html
