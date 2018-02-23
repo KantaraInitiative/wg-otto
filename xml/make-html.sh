f598625dfbace2235a200935a3ef7961d990599d
Mike Schwartz <mike@gluu.org>
2018-02-23 12:28:09 -0600

    Fixed typo in make-html.sh script

--- xml/make-html.sh
+++ xml/make-html.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-xsltproc rfc2629.xslt otto-vocab-01.xml > ../html/otto-vocab-1.0.html
+xsltproc rfc2629.xslt otto-vocab-01.xml > ../html/otto-core-1.0.html
 
 xsltproc rfc2629.xslt otto-api-01.xml > ../html/otto-api-1.0.html
 
 xsltproc rfc2629.xslt otto-openid-1.0.xml > ../html/otto-openid-1.0.html
 
-xsltproc rfc2629.xslt otto-saml-1.0.xml > ../html/otto-openid-1.0.html
-
+# xsltproc rfc2629.xslt otto-saml-1.0.xml > ../html/otto-saml-1.0.html
